70aa5c7a3b2d58ecfc8025131761dc9e6685eb7d
Nathan Smith <smith.11170@outlook.com>
2025-11-03 19:22:31 -0500

    Working on command generation for system strategy

--- nob++.cxx
+++ nob++.cxx
@@ -1,11 +1,18 @@
 import nob;
+import std;
 
 int main(int argc, char* argv[])
 {
-	auto [err, result] = nob::initBuilder()
-		.useDefaultArgumentHandling()
-		.detectCompiler()
-		.useBuildExecutor(nob::ExecutorType::System)
-		.setWarningLevel(nob::WarningLevel::Strict)
-		.build("exercises/hello/hello.cxx");
+	auto [err, result] = nob::Builder(argc, argv)
+													 .useDefaultArgumentHandling()
+													 .useCompiler(nob::Compiler::MSVC)
+													 .useExecutionStrategy(nob::ExecutionStrategy::System)
+													 .useWarningLevel(nob::WarningLevel::Strict)
+													 .build("exercises/hello/hello.cxx");
+	if (err.has_value())
+	{
+		std::println("RuntimeError: {}", nob::getRuntimeErrorMessage(err.value()));
+		return 1;
+	}
+	return 0;
 }
