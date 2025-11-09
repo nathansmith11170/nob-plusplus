d5cfc86ed48ba6affd21504701234cd849c41c98
Nathan Smith <smith.11170@outlook.com>
2025-11-09 16:23:09 -0500

    Implement some of the command generation logic for the executable target

--- nob++.cxx
+++ nob++.cxx
@@ -6,9 +6,7 @@ int main(int argc, char* argv[])
 	auto [err, result] = nob::Builder(argc, argv)
 													 .useDefaultArgumentHandling()
 													 .useCompiler(nob::Compiler::MSVC)
-													 .useExecutionStrategy(nob::ExecutionStrategy::System)
-													 .useWarningLevel(nob::WarningLevel::Strict)
-													 .build("exercises/hello/hello.cxx");
+													 .buildExe("exercises/hello/hello.cxx");
 	if (err.has_value())
 	{
 		std::println("RuntimeError: {}", nob::getRuntimeErrorMessage(err.value()));
